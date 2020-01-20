fe25a99c6aa3b50807a21de3783bcff6b154e3f5
Pedro Filho <pedro@filho.me>
2020-01-20 18:31:52 +0000

    fixing height

--- src/Provider.js
+++ src/Provider.js
@@ -7,21 +7,16 @@ const Provider = props => {
   const state = useDeck();
 
   return (
-    <div
-      style={{
-        fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol"`
-      }}
-    >
-      <div
+    <>
+      <main
         style={{
-          width: "100vw",
-          height: "946px"
+          fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol"`
         }}
       >
         {props.children}
-      </div>
+      </main>
       <Steps length={state.length} position={state.index + 1} />
-    </div>
+    </>
   );
 };
 
