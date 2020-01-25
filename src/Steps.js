fe5c67af85f7ae698a731a62f9e70fc9e7de83f1
Pedro Filho <pedro@filho.me>
2020-01-25 14:20:58 +0000

    adding pacman

--- src/Steps.js
+++ src/Steps.js
@@ -12,33 +12,50 @@ const Steps = ({ length, position }) => {
     <div
       style={{
         position: "fixed",
-        bottom: 0,
-        margin: "10px 0",
+        bottom: 10,
         display: "flex",
         width: "100%",
         justifyContent: "center",
+        alignItems: "center",
         zIndex: 1
       }}
     >
-      {fillArrayWithNumbers(position).map(i => (
-        <span
+      {fillArrayWithNumbers(position).map(
+        i =>
+          i === 0 && (
+            <div
+              key={`passed-${i}`}
+              style={{
+                height: 15,
+                width: 15,
+                margin: 2,
+                backgroundColor: "#000",
+                borderRadius: "50%",
+                position: "relative",
+                border: "none"
+              }}
+            >
+              {!!(position % 2) && (
+                <div
+                  style={{
+                    background: "white",
+                    position: "absolute",
+                    width: "100%",
+                    height: "100%",
+                    clipPath: "polygon(120% 75%, 50% 50%, 120% 20%)",
+                    border: "white"
+                  }}
+                />
+              )}
+            </div>
+          )
+      )}
+      {fillArrayWithNumbers(length - position).map(i => (
+        <div
           key={`toPass-${i}`}
           style={{
-            height: 8,
-            width: 8,
-            margin: 2,
-            backgroundColor: "#000",
-            borderRadius: "50%",
-            display: "inline-block"
-          }}
-        />
-      ))}
-      {fillArrayWithNumbers(length - position).map(i => (
-        <span
-          key={`passed-${i}`}
-          style={{
-            height: 8,
-            width: 8,
+            height: 5,
+            width: 5,
             margin: 2,
             backgroundColor: "#bbb",
             borderRadius: "50%",
